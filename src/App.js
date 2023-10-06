2e7654dcef7ababdcafe2c70bc08c7e316890eef
Aysa <aysabar92@gmail.com>
2023-10-06 18:29:04 -0400

    lesson-1-2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-const todoList = [
-    {
-      id:'1',
-      title:'Walk the dog',
-    },
-
-    {
-      id:'2',
-      title:'Clean my car',
-    },
-
-    {
-      id:'3',
-      title:'Finish Homework',
-    }
-
-];
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
 
-      <ul>
-        {todoList.map(function (item) {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
+       <AddTodoForm/> 
+      <TodoList/>
+      
       
     </div>
   );
---
+++ src/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const todoList = [
+    {
+      id:'1',
+      title:'Walk the dog',
+    },
+
+    {
+      id:'2',
+      title:'Clean my car',
+    },
+
+    {
+      id:'3',
+      title:'Finish Homework',
+    }
+
+];
+
+const TodoList = () => {
+    return (
+        <ul>
+        {todoList.map(function (item) {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
+
+    ) 
+}
+
+export default TodoList;
